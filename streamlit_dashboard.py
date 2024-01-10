38c94fcec24db5205d86fe2ca4074e6cf7515fdc
Leandro da Silva de Souza <leandro.souza.159@gmail.com>
2024-01-10 19:14:39 -0300

    Estilizando dataframe

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -7,12 +7,15 @@ st.set_page_config(page_title = 'Análise Financeira Mensal', layout = 'wide', p
 # Carregamento dos dados
 months = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
 years = {'2023': '1quM2SQCyXHGd5zPc3y46FYjql_VoOQUqqwRtF9Ws0Xg', '2024': '1M-UIHenSns7o_iSTODK047YOO3Hh-nLS9DLaLvmwU7g'}
-col1, col2 = st.columns(2)
-with col1:
-    sheet_name = st.selectbox('Mês', months)
-with col2:
-    year = st.selectbox('Ano', tuple(years.keys()))
-    sheet_id = years[year]
+
+sheet_name = st.sidebar.selectbox('Mês', months)
+year = st.sidebar.selectbox('Ano', tuple(years.keys()))
+# col1, col2 = st.columns(2)
+# with col1:
+#     sheet_name = st.selectbox('Mês', months)
+# with col2:
+#     year = st.selectbox('Ano', tuple(years.keys()))
+sheet_id = years[year]
 url = f'https://docs.google.com/spreadsheet/ccc?key={sheet_id}&output=xlsx'
 df = pd.read_excel(url, sheet_name=sheet_name, header=2).iloc[:, 10:14]
 planejado_real = pd.read_excel(url, sheet_name=sheet_name, header=2).iloc[0:7, 0:3]
@@ -38,7 +41,7 @@ economizado = ((guardar/valor_disponivel)*100)
 
 st.markdown("<br>", unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([0.465, 0.445, 0.09])  # Adjust column widths
+col1, col2, col3 = st.columns([0.3, 0.3, 2])  # Adjust column widths
 
 with col1:
     st.metric('Valor economizado', f'{economizado:.2f}%'.replace('.', ','))
@@ -62,17 +65,28 @@ fig = px.bar(total_categoria.reset_index(),
              color_discrete_sequence=['#3498DB'])
 
 # Adiciona o formato desejado para os valores de y e remove os valores no eixo y
-fig.update_traces(texttemplate='R$%{y:.0f}', 
-                  textposition='outside', 
-                  hovertemplate='')
+fig.update_traces(texttemplate='R$ %{y:.0f}', 
+                  textposition='outside',
+                  hoverinfo='skip', 
+                  hovertemplate=None)
 fig.update_layout(showlegend=False, yaxis=dict(title='', tickvals=[]))
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([2, 1])
 with col1:
     st.plotly_chart(fig, use_container_width=True)
 with col2:
     st.markdown("<div style='margin-top: 35px;'><b>Planejado vs Realizado<br></div><br>", unsafe_allow_html=True)
-    st.dataframe(planejado_real, use_container_width=True)
+    
+    def highlight_red(row):
+        if row['Realizado'] > row['Planejado']:
+            return ['', 'color: red']
+        else:
+            return ['', 'color: green']
+
+    planejado_real.rename(columns={'Real': 'Realizado'}, inplace=True)
+    styled_planejado_real = planejado_real.style.apply(highlight_red, axis=1)
+    styled_planejado_real = styled_planejado_real.format({'Planejado': 'R$ {:.0f}', 'Realizado': 'R$ {:.0f}'}, decimal = ',')
+    st.dataframe(styled_planejado_real, use_container_width=True)
 
 # Métricas dos custos médios para subcategorias relevantes
 mask = ((df['Descrição'] == 'Uber') | (df['Descrição'] == '99POP')) & ((df['Valor'] > 25) | (df['Valor'] < 14))
@@ -80,12 +94,12 @@ df_transporte_trabalho = df[~mask]
 media_categoria = df_transporte_trabalho[['Valor', 'Descrição']].groupby('Descrição').mean()
 categorias_desejadas = ['Almoço', 'Jantar', 'Café', '99POP', 'Uber']
 categorias_presentes = media_categoria.index.isin(categorias_desejadas)
-media_categoria = media_categoria[categorias_presentes].T.style.format('R${:.2f}', decimal= ',')
+media_categoria = media_categoria[categorias_presentes].T.style.format('R$ {:.2f}', decimal= ',')
 
 table1, table2 = st.columns(2)
 with table1:
     st.markdown('<b>Tabela de dados completa', unsafe_allow_html=True)
-    st.dataframe(df[['Valor', 'Descrição', 'Categoria']].style.format({'Valor': 'R${:.2f}'}, decimal = ','), 
+    st.dataframe(df[['Valor', 'Descrição', 'Categoria']].style.format({'Valor': 'R$ {:.2f}'}, decimal = ','), 
                  use_container_width = True, 
                  hide_index = True, 
                  height = 750)
