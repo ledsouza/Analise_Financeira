4d5ff1f5b442ea0560c4ea1ae67d3507505ac5f4
Leandro da Silva de Souza <leandro.souza.159@gmail.com>
2024-01-13 09:03:33 -0300

    Incluído a opção de pegar a data atual ao carregar a página

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -1,21 +1,27 @@
+"""
+Este script cria um painel do Streamlit para análise financeira mensal.
+Ele carrega dados de uma planilha do Google, realiza análises nos dados e visualiza os resultados.
+"""
+
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+from datetime import datetime
 
-st.set_page_config(page_title = 'Análise Financeira Mensal', layout = 'wide', page_icon = "heavy_dollar_sign")
+# Configuração da página do Streamlit
+st.set_page_config(page_title='Análise Financeira Mensal', layout='wide', page_icon="heavy_dollar_sign")
 
 # Carregamento dos dados
 months = ('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro')
 years = {'2023': '1quM2SQCyXHGd5zPc3y46FYjql_VoOQUqqwRtF9Ws0Xg', '2024': '1M-UIHenSns7o_iSTODK047YOO3Hh-nLS9DLaLvmwU7g'}
 
-sheet_name = st.sidebar.selectbox('Mês', months)
-year = st.sidebar.selectbox('Ano', tuple(years.keys()))
-# col1, col2 = st.columns(2)
-# with col1:
-#     sheet_name = st.selectbox('Mês', months)
-# with col2:
-#     year = st.selectbox('Ano', tuple(years.keys()))
+default_year = tuple(years.keys()).index(str(datetime.now().year))
+year = st.sidebar.selectbox('Ano', tuple(years.keys()), index=default_year)
 sheet_id = years[year]
+
+default_month = datetime.now().month - 1
+sheet_name = st.sidebar.selectbox('Mês', months, index=default_month)
+
 url = f'https://docs.google.com/spreadsheet/ccc?key={sheet_id}&output=xlsx'
 df = pd.read_excel(url, sheet_name=sheet_name, header=2).iloc[:, 10:14]
 planejado_real = pd.read_excel(url, sheet_name=sheet_name, header=2).iloc[0:7, 0:3]
@@ -41,7 +47,7 @@ economizado = ((guardar/valor_disponivel)*100)
 
 st.markdown("<br>", unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([0.3, 0.3, 2])  # Adjust column widths
+col1, col2, col3 = st.columns([0.3, 0.3, 2])  # Ajustar larguras das colunas
 
 with col1:
     st.metric('Valor economizado', f'{economizado:.2f}%'.replace('.', ','))
@@ -58,13 +64,13 @@ st.markdown("<br>", unsafe_allow_html=True)
 fig = px.bar(total_categoria.reset_index(), 
              x='Categoria', 
              y='Valor', 
-             title = 'Valor total por categoria', 
+             title='Valor total por categoria', 
              labels={'Categoria': '', 'Valor': 'Valor total (R$)'},
              text_auto=True, 
-             color = 'Categoria', 
+             color='Categoria', 
              color_discrete_sequence=['#3498DB'])
 
-# Adiciona o formato desejado para os valores de y e remove os valores no eixo y
+# Ajuste no display de informações
 fig.update_traces(texttemplate='R$ %{y:.0f}', 
                   textposition='outside',
                   hoverinfo='skip', 
@@ -85,7 +91,7 @@ with col2:
 
     planejado_real.rename(columns={'Real': 'Realizado'}, inplace=True)
     styled_planejado_real = planejado_real.style.apply(highlight_red, axis=1)
-    styled_planejado_real = styled_planejado_real.format({'Planejado': 'R$ {:.0f}', 'Realizado': 'R$ {:.0f}'}, decimal = ',')
+    styled_planejado_real = styled_planejado_real.format({'Planejado': 'R$ {:.0f}', 'Realizado': 'R$ {:.0f}'}, decimal=',')
     st.dataframe(styled_planejado_real, use_container_width=True)
 
 # Métricas dos custos médios para subcategorias relevantes
@@ -94,16 +100,16 @@ df_transporte_trabalho = df[~mask]
 media_categoria = df_transporte_trabalho[['Valor', 'Descrição']].groupby('Descrição').mean()
 categorias_desejadas = ['Almoço', 'Jantar', 'Café', '99POP', 'Uber']
 categorias_presentes = media_categoria.index.isin(categorias_desejadas)
-media_categoria = media_categoria[categorias_presentes].T.style.format('R$ {:.2f}', decimal= ',')
+media_categoria = media_categoria[categorias_presentes].T.style.format('R$ {:.2f}', decimal=',')
 
 table1, table2 = st.columns(2)
 with table1:
     st.markdown('<b>Tabela de dados completa', unsafe_allow_html=True)
-    st.dataframe(df[['Valor', 'Descrição', 'Categoria']].style.format({'Valor': 'R$ {:.2f}'}, decimal = ','), 
-                 use_container_width = True, 
-                 hide_index = True, 
-                 height = 750)
+    st.dataframe(df[['Valor', 'Descrição', 'Categoria']].style.format({'Valor': 'R$ {:.2f}'}, decimal=','),
+                 use_container_width=True,
+                 hide_index=True,
+                 height=750)
 with table2:
     st.markdown('<b>Custo médio por subcategoria', unsafe_allow_html=True)
-    st.dataframe(media_categoria, use_container_width = True)
+    st.dataframe(media_categoria, use_container_width=True)
     st.image('Spending_Plan.png')
